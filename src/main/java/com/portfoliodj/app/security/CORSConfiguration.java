59ff8d72b9c61de47cfaae640f97a840d8787865
Fewatts <feralveswatts@gmail.com>
2023-06-16 17:31:15 -0300

    Task 6
    All set! c:

---
+++ src/main/java/com/portfoliodj/app/security/CORSConfiguration.java
@@ -0,0 +1,19 @@
+package com.portfoliodj.app.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/customers/register") 
+            .allowedOrigins("https://pvtonissessions.vercel.app") 
+            .allowedMethods("POST", "OPTIONS") 
+            .allowedHeaders("*") 
+            .allowCredentials(true) 
+            .maxAge(3600); 
+    }
+}
